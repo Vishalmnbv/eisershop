aa39cad61c4a9f5d1ca14032ad52f6fe2f66f1e4
vishal <gajjarvishal288@gmail.com>
2026-09-10 21:03:36 +0530

    temporarily enable debug mode

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz4-ucd7u4_&p2d(h&(khf$1sc2&&%i%qdhdupp@*frac_recc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
