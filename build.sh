02cf0bce17f2276986a2b8c46ee8c5d56646f857
vishal <gajjarvishal288@gmail.com>
2026-09-10 18:14:30 +0530

    Fix build script for safe data loading and superuser creation

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+python manage.py loaddata datadump.json
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
