c98fa31a3fe1d3177d84661c4375ac835265f85c
vishal <gajjarvishal288@gmail.com>
2026-09-15 17:53:52 +0530

    Fix email settings for Resend

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -153,13 +153,12 @@ REST_FRAMEWORK = {
 
 # Email Setup
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.resend.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "gajjarvishal288@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "sncrsnpznyoyznaw")
-DEFAULT_FROM_EMAIL =  EMAIL_HOST_USER
+EMAIL_HOST_USER = "resend"
+EMAIL_HOST_PASSWORD = os.getenv("RESEND_API_KEY")
+DEFAULT_FROM_EMAIL = "onboarding@resend.dev"
 
 
 CLOUDINARY_STORAGE = {
