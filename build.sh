6529088ac12cd924830c93377320f4c90c1c31b9
vishal <gajjarvishal288@gmail.com>
2026-09-16 16:43:30 +0530

    Force explicit pip install in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Upgrade pip and install dependencies explicitly
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
