f9b86e54522bf5307a79f366ffec652ea5899e7e
vishal <gajjarvishal288@gmail.com>
2026-09-16 17:28:03 +0530

    Fix productmrpprice attribute name in utils.py

--- app1/utils.py
+++ app1/utils.py
@@ -19,37 +19,34 @@ def apply_rating(product):
     product.full_stars_range = range(full_stars)
     product.half_stars_range = range(half_stars)
     product.empty_stars_range = range(empty_stars)
+    
+    # Yahan productmrp ki jagah productmrpprice kiya gaya hai
     if (
-        product.productmrp
+        product.productmrpprice
         and product.productprice
-        and product.productmrp > product.productprice
+        and product.productmrpprice > product.productprice
     ):
         discount = (
-            (product.productmrp - product.productprice)
-            / product.productmrp
+            (product.productmrpprice - product.productprice)
+            / product.productmrpprice
         ) * 100
         product.productdiscountrate = str(int(discount))
     else:
         product.productdiscountrate = "0"
     return product
-
 def send_async_login_email(user, html_content):
     try:
         configuration = sib_api_v3_sdk.Configuration()
         configuration.api_key['api-key'] = os.getenv("BREVO_API_KEY")
-
         api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
-        
         sender = {"name": "EiserShop", "email": os.getenv("DEFAULT_FROM_EMAIL")}
         to = [{"email": user.email, "name": user.username}]
-        
         send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
             to=to,
             sender=sender,
             subject="Login Successful - EiserShop",
             html_content=html_content
         )
-
         api_instance.send_transac_email(send_smtp_email)
         print("Brevo HTTP API Email sent successfully to", user.email)
     except ApiException as e:
